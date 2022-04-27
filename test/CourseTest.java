dc52011bafb0394600e4a2243662b243e8e9120b
Robert Kaiser <>
2022-04-27 10:10:25 -0700

    Test1

--- test/CourseTest.java
+++ test/CourseTest.java
@@ -6,18 +6,5 @@ import static org.junit.Assert.assertEquals;
 //Test
 public class CourseTest {
     ArrayList studentsInCourse= new ArrayList();
-    @Test
-    public void numberOfStudentsInCourseTest(){
-        Course aep = new Course("aep", studentsInCourse);
-        ArrayList chosenCourses = new ArrayList<>();
-        chosenCourses.add(aep);
-        Student Jan = new Student("BusinessInformatics", "visitingResearcher",chosenCourses,null );
-        Student Robert = new Student("BusinessInformatics", "visitingResearcher",chosenCourses,null );
-        Student Rae = new Student("BusinessInformatics", "visitingResearcher",chosenCourses,null );
-        aep.addStudentsToCourse(Jan);
-        aep.addStudentsToCourse(Robert);
-        aep.addStudentsToCourse(Rae);
 
-        assertEquals(3, aep.numberOfStudentsInCourse());
-    }
 }
--- test/Student_Test.java
+++ test/Student_Test.java
@@ -7,5 +7,15 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 //Test
 public class Student_Test {
-
+    @Test
+    public void studentShouldBeABusinessInformaticsMajor(){
+        ArrayList coursesChosen = new ArrayList<>();
+        ArrayList attendedLectures = new ArrayList<>();
+        ArrayList studentsInCourse = new ArrayList<>();
+        Course aep = new Course("aep", studentsInCourse);
+        Lecture aep20220422 = new Lecture(LocalTime.of(12,30), LocalDate.of(2022,04,22),aep);
+        Student jan = new Student("businessInformatics", "VisitingResearcher", coursesChosen, attendedLectures);
+        jan.attendLecture(aep20220422);
+        assertEquals(aep20220422, jan.attendedLectures.get(0));
+    }
 }
